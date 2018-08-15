9c989385f94a4acb1d8cf72a5415b5431d38a954
WillLinIFM <niceconcert@gmail.com>
2018-08-15 16:21:39 +0800

    update

--- js/Scene.js
+++ js/Scene.js
@@ -5,17 +5,21 @@
         // Creates, angles, distances and targets the camera
 	    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 0, new BABYLON.Vector3(0 , 150, 0), scene);
         // This positions the camera
-        scene.activeCamera = camera;
+        scene.activeCamera = camera;  
         camera.setPosition(new BABYLON.Vector3(0 , 150, -150));    
         // This attaches the camera to the canvas
         camera.attachControl(canvas, false);
         camera.lowerRadiusLimit = camera.upperRadiusLimit = camera.radius;
         var light1_front = new BABYLON.HemisphericLight("Omni1", new BABYLON.Vector3(0, 1, -1), scene);
         scene.beforeCameraRender = function () {
-        light1_front.intensity = 0.7;        
+        light1_front.intensity = 1.6;        
         };
+
         engine.runRenderLoop(function () {
                     scene.render();
+//                    scene.onPointerUp = function(){
+//                   console.log(camera.position);
+//                }
             })
         }else{
         alert("WebGL not supported in this browser.");
@@ -24,15 +28,51 @@
 
 
 
+
 function updateshapekey(sa){
     var shapekeyslide = new QI.VertexDeformation("ENTIRE_MESH","BASIS",["FQYDA"],[sa],1);
         ctestMI.queueSingleEvent(shapekeyslide);
-        ctestMI2.queueSingleEvent(shapekeyslide);
+        ctestMI2.queueSingleEvent(shapekeyslide);    
+//    var shapekeyslide = new QI.VertexDeformation("ENTIRE_MESH","BASIS",["KEY 1"],[sa],1);
+//        ctestMI.queueSingleEvent(shapekeyslide);
+        //ctestMI2.queueSingleEvent(shapekeyslide);
 }
     
 
 
+function test_MaDecoder(){
+        $.getScript("tag_randomvalue.js",function(){
+    ctest = new tag_randomvalue.MeshFactory(scene);
+    ctestMI = ctest.instance("shirt0702")
+    ctestMI.position = new BABYLON.Vector3(0,50,20);
+    var shapekeyslide = new QI.VertexDeformation("ENTIRE_MESH","BASIS",["KEY 1"],[0.01],1);
+    ctestMI.queueSingleEvent(shapekeyslide);
+    //Camera Aniamtion
+    var positionAnimation = new BABYLON.Animation("camerarotate", "alpha", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+    var positionAnimation2 = new BABYLON.Animation("cameraposition", "radius", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
 
+    var keys1 = [{
+        frame : 0,
+        value : -1
+        }, {
+        frame : 100,
+        value : 4.7
+        }];
+    var keys2 = [{
+        frame : 0,
+        value : 50
+        }, {
+        frame : 100,
+        value : 150
+        }];
+
+    positionAnimation.setKeys(keys1);
+    positionAnimation2.setKeys(keys2);
+    camera.animations.push(positionAnimation);
+    camera.animations.push(positionAnimation2);
+    scene.beginAnimation(camera, 0, 100, false, 1);
+        })
+}
 
 function teston(){
     
@@ -41,6 +81,7 @@ function teston(){
     ctestMI = ctest.instance("152QA_bone")
     ctestMI.position = new BABYLON.Vector3(0,50,20);
     console.log(scene);
+    console.log(window);
     var shapeKeyGroup = ctestMI.getShapeKeyGroup("ENTIRE_MESH");
      if(shapeKeyGroup) ctestMI.removeShapeKeyGroup("ENTIRE_MESH");
     $.getScript("avatar_anim_FQYDA.js",function(){
@@ -100,6 +141,8 @@ function teston(){
 
 $.getScript("cloth_anim.js",function(){
     ctest2 = new cloth_anim.MeshFactory(scene);
+    //decode
+    
     ctestMI2 = ctest2.instance("FQBasis")
     ctestMI2.position = new BABYLON.Vector3(0,50,20);
     console.log(scene);
@@ -155,13 +198,15 @@ $.getScript("cloth_anim.js",function(){
 //    console.log(ctestM);
 //    var uvs = ctestM.getVerticesData()
     //scene.beginAnimation(scene.getSkeletonByName("152QA_bone"), 0, 80, true, 0.8);
-    scene.beginAnimation(scene.getSkeletonByName("152QA_bone"), 0, 80, true, 0.8);
-    scene.beginAnimation(scene.getSkeletonByName("FQBasis"), 0, 80, true, 0.8);
+//    scene.beginAnimation(scene.getSkeletonByName("152QA_bone"), 0, 80, true, 0.8);
+//    scene.beginAnimation(scene.getSkeletonByName("FQBasis"), 0, 80, true, 0.8);
     
 })  
 }
 
 function testoff(){
-    if(scene.getMeshByID("152QA_bone")) ctestM.dispose(false,true);}
+    if(scene.getMeshByID("152QA_bone")) ctestMI.dispose(false,true);
+    if(scene.getMeshByID("FQBasis")) ctestMI2.dispose(false,true);
+}
     //window.dazavatar_dress1.dazavatar_dress1 = null;
     
